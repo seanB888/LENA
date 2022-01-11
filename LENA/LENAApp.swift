5b85673e51b2c3545bae932fff7b0bf73d83bc4c
Sean Blake <sean.blake97@outlook.com>
2022-01-11 12:51:01 -0500

    Initial Commit

---
+++ LENA/LENAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LENAApp.swift
+//  LENA
+//
+//  Created by SEAN BLAKE on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LENAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
